c1d08b17514ad20eb5d13e0ece103749fe8613c1
Sitx Basterg <SitxBasterg@inbox.ru>
2020-11-30 19:27:54 +0500

    30/11/20 added getExcursionByID.go and updated addExcursion.go

--- addExcursion/addExcursion.go
+++ addExcursion/addExcursion.go
@@ -11,17 +11,16 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
-
 type Excursion struct {
 	ExcursionID  string `json:"excursionID"`
 	CountryUpper string `json:"countryUpper"`
-	Country		 string `json:"country"`
+	Country      string `json:"country"`
 	Description  string `json:"description"`
-	TownUpper	 string `json:"townUpper"`
-	Town 		 string `json:"town"`
+	TownUpper    string `json:"townUpper"`
+	Town         string `json:"town"`
 }
 
-func Handler(request events.APIGatewayProxyRequest)(events.APIGatewayProxyResponse, error) {
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	sess := session.Must(session.NewSession())
 	ddb := dynamodb.New(sess)
 
@@ -31,37 +30,34 @@ func Handler(request events.APIGatewayProxyRequest)(events.APIGatewayProxyRespon
 	json.Unmarshal([]byte(body), &thisExcursion)
 
 	var excursionToAdd = map[string]string{
-		"PK":  	 	   fmt.Sprintf("EXC#ID%s", thisExcursion.ExcursionID),
-		"SK": 	 	   fmt.Sprintf("EXC#ID%s", thisExcursion.ExcursionID),
-		"GSI1PK":  	   thisExcursion.CountryUpper,
-		"GSI1SK":  	   fmt.Sprintf("%s#EXC#ID%s",thisExcursion.TownUpper, thisExcursion.ExcursionID),
-		"Country": 	   thisExcursion.Country,
+		"PK":          fmt.Sprintf("EXC#ID%s", thisExcursion.ExcursionID),
+		"SK":          fmt.Sprintf("EXC#ID%s", thisExcursion.ExcursionID),
+		"GSI1PK":      thisExcursion.CountryUpper,
+		"GSI1SK":      fmt.Sprintf("%s#EXC#ID%s", thisExcursion.TownUpper, thisExcursion.ExcursionID),
+		"Country":     thisExcursion.Country,
 		"Description": thisExcursion.Description,
-		"Town": 	   thisExcursion.Town,
+		"Town":        thisExcursion.Town,
+		"ExcursionID": thisExcursion.ExcursionID,
 	}
 
 	av, err := dynamodbattribute.MarshalMap(excursionToAdd)
 	if err != nil {
-		return events.APIGatewayProxyResponse{Body: "Post error", StatusCode: 500}, nil
+		return events.APIGatewayProxyResponse{Body: "Marshalling error", StatusCode: 500}, nil
 	}
 
 	input := &dynamodb.PutItemInput{
-		Item:      			 av,
+		Item:                av,
 		ConditionExpression: aws.String("attribute_not_exists(PK)"),
-		TableName: 			 aws.String("Excursions"),
+		TableName:           aws.String("Excursions"),
 	}
 
 	_, err = ddb.PutItem(input)
 	if err != nil {
-		return events.APIGatewayProxyResponse{Body: "Put error" , StatusCode: 200}, nil
+		return events.APIGatewayProxyResponse{Body: "Put error", StatusCode: 200}, nil
 	}
-	return events.APIGatewayProxyResponse{Body: "Excursion added" , StatusCode: 200}, nil
-
+	return events.APIGatewayProxyResponse{Body: "Excursion added", StatusCode: 200}, nil
 }
 
-func main()  {
+func main() {
 	lambda.Start(Handler)
 }
-
-
-
---
+++ getExcursion/getExcursionByID.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
+)
+
+type Excursion struct {
+	ExcursionID string
+	Country     string
+	Description string
+	Town        string
+}
+
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	sess := session.Must(session.NewSession())
+	ddb := dynamodb.New(sess)
+
+	var thisExcursion Excursion
+
+	var excID string
+	excID = request.QueryStringParameters["excID"]
+
+	proJ := expression.NamesList(
+		expression.Name("ExcursionID"), expression.Name("Country"),
+		expression.Name("Description"), expression.Name("Town"))
+
+	expr, err := expression.NewBuilder().WithProjection(proJ).Build()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	input := &dynamodb.GetItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"PK": {
+				S: aws.String(fmt.Sprintf("EXC#ID%s", excID)),
+			},
+			"SK": {
+				S: aws.String(fmt.Sprintf("EXC#ID%s", excID)),
+			},
+		},
+		ExpressionAttributeNames: expr.Names(),
+		ProjectionExpression:     expr.Projection(),
+		TableName:                aws.String("Excursions"),
+	}
+
+	result, err := ddb.GetItem(input)
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: "Get - error", StatusCode: 200}, nil
+	}
+
+	if result.Item == nil {
+		return events.APIGatewayProxyResponse{Body: "Can't get the excursion", StatusCode: 200}, nil
+	}
+
+	err = dynamodbattribute.UnmarshalMap(result.Item, &thisExcursion)
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: "Unmarshalling error", StatusCode: 200}, nil
+	}
+
+	var excursionToSend = map[string]string{
+		"excursionID": thisExcursion.ExcursionID,
+		"country":     thisExcursion.Country,
+		"description": thisExcursion.Description,
+		"town":        thisExcursion.Town,
+	}
+
+	rez, _ := json.Marshal(excursionToSend)
+
+	return events.APIGatewayProxyResponse{Body: string(rez), StatusCode: 200}, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
